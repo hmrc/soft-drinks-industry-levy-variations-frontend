fddee897e9eb49ac7e3da4e582119c3b5169e840
JakeReid2020 <75027964+JakeReid2020@users.noreply.github.com>
2023-03-23 15:37:52 +0000

    [DLS-7020] Config Improvements

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,20 @@ import sbt._
 
 object AppDependencies {
   import play.core.PlayVersion
-  val bootstrapVersion = "7.12.0"
+  val bootstrapVersion = "7.13.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.6.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.13.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-language"                  % "6.1.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
     "org.typelevel"     %% "cats-core"                      % "2.9.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % bootstrapVersion,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion,
     "org.scalatest"           %% "scalatest"               % "3.2.15",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
