2c948365913218b6c4e724b0a34b2e2ec8f97692
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-03-31 11:13:25 +0100

    DLS-7528 update appdependencies and readme as per platops pr bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.6.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-language"                  % "6.1.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0",
     "org.typelevel"     %% "cats-core"                      % "2.9.0"
   )
