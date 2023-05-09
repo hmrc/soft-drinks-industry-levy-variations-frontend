c5fb95ee9179545097ca4dcfe91fdfbe214055ad
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-05-09 09:01:11 +0100

    DLS-8013 added it tests to scaffold

---
+++ it/testSupport/databases/SessionDatabaseOperations.scala
@@ -0,0 +1,31 @@
+package testSupport.databases
+
+import models.UserAnswers
+import repositories.SessionRepository
+import testSupport.TestConfiguration
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+trait SessionDatabaseOperations {
+
+  self: TestConfiguration =>
+
+  val sessionRespository: SessionRepository
+
+  def setAnswers(userAnswers: UserAnswers)(implicit timeout: Duration): Unit = Await.result(
+    sessionRespository.set(userAnswers),
+    timeout
+  )
+
+  def getAnswers(id: String)(implicit timeout: Duration): Option[UserAnswers] = Await.result(
+    sessionRespository.get(id),
+    timeout
+  )
+
+  def remove(id: String)(implicit timeout: Duration): Boolean = Await.result(
+    sessionRespository.clear(id),
+    timeout
+  )
+
+}
