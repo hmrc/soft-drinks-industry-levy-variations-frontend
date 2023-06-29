fe2f5deae1493446a58223b7465cfe1f89ecf504
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-06-29 11:03:49 +0100

    DLS-7035: Disallows remove behaviour for warehouse list size 1

--- app/views/summary/changeActivity/SecondaryWarehouseDetailsSummary.scala
+++ app/views/summary/changeActivity/SecondaryWarehouseDetailsSummary.scala
@@ -29,23 +29,31 @@ import viewmodels.implicits._
 
 object SecondaryWarehouseDetailsSummary  {
 
-  def row(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
+  def cyaRow(answers: UserAnswers)(implicit messages: Messages): Option[SummaryListRow] =
     answers.get(WarehouseDetailsPage).map {
       answer =>
 
         val value = if (answer) "site.yes" else "site.no"
 
         SummaryListRowViewModel(
-          key     = "updateRegisteredDetails.warehouseDetails.checkYourAnswersLabel",
+          key     = "changeActivity.secondaryWarehouseDetails.checkYourAnswersLabel",
           value   = ValueViewModel(value),
           actions = Seq(
             ActionItemViewModel("site.change", routes.WarehouseDetailsController.onPageLoad(CheckMode).url)
-              .withVisuallyHiddenText(messages("updateRegisteredDetails.warehouseDetails.change.hidden"))
+              .withVisuallyHiddenText(messages("changeActivity.secondaryWarehouseDetails.change.hidden"))
           )
         )
     }
 
-  def row2(warehouseList: Map[String, Warehouse])(implicit messages: Messages): List[SummaryListRow] = {
+  def summaryRows(warehouseList: Map[String, Warehouse], noRemoveAction: Boolean = false)(implicit messages: Messages): List[SummaryListRow] = {
+    val actions: Option[Actions] = if (noRemoveAction) {
+      None
+    } else {
+      Some(Actions("", Seq(
+        ActionItemViewModel("site.remove", routes.WarehouseDetailsController.onPageLoad(NormalMode).url)
+          .withVisuallyHiddenText(messages("changeActivity.secondaryWarehouseDetails.remove.hidden"))
+      )))
+    }
     warehouseList.map {
           warehouse =>
             SummaryListRow(
@@ -53,10 +61,7 @@ object SecondaryWarehouseDetailsSummary  {
                 content = HtmlContent(AddressFormattingHelper.addressFormatting(warehouse._2.address, warehouse._2.tradingName)),
                 classes = "govuk-!-font-weight-regular govuk-!-width-full"
               ),
-              actions = Some(Actions("",Seq(
-                ActionItemViewModel("site.remove", routes.WarehouseDetailsController.onPageLoad(NormalMode).url)
-                  .withVisuallyHiddenText(messages("updateRegisteredDetails.warehouseDetails.remove.hidden"))
-              )))
+              actions = actions
             )
           }.toList
     }
