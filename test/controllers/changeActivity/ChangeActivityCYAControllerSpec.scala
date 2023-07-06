11aa327d4af0c36146a7a25cf8e2b1285f662255
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-07-06 12:49:25 +0100

    DLS-7029: Generative tests for ChangeActivity CYA Controller

--- test/controllers/changeActivity/ChangeActivityCYAControllerSpec.scala
+++ test/controllers/changeActivity/ChangeActivityCYAControllerSpec.scala
@@ -19,6 +19,10 @@ package controllers.changeActivity
 import base.SpecBase
 import controllers.changeActivity.routes._
 import models.SelectChange.ChangeActivity
+import models.changeActivity.AmountProduced
+import models.changeActivity.AmountProduced.{Large, None => NoneProduced, Small}
+import models.{LitresInBands, UserAnswers}
+import pages.changeActivity._
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
@@ -31,46 +35,121 @@ class ChangeActivityCYAControllerSpec extends SpecBase with SummaryListFluency {
 
   "Check Your Answers Controller" - {
 
-    "must return OK and the correct view for a GET" in {
-      
-      val userAnswers = emptyUserAnswersForChangeActivity
-      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+//    TODO: Refactor this if possible
+    def getUserAnswers(
+                        amountProduced: Option[AmountProduced] = None,
+                        thirdPartyPackaging: Option[Boolean] = None,
+                        ownBrands: Option[Boolean] = None,
+                        contract: Option[Boolean] = None,
+                        imports: Option[Boolean] = None
+                      ): UserAnswers = {
+      val initialUserAnswers = emptyUserAnswersForChangeActivity
+      val userAnswersWithAmountProduced = amountProduced match {
+        case Some(Large) => initialUserAnswers.set(AmountProducedPage, Large).success.value
+        case Some(Small) => initialUserAnswers.set(AmountProducedPage, Small).success.value
+        case Some(NoneProduced) => initialUserAnswers.set(AmountProducedPage, NoneProduced).success.value
+        case None => initialUserAnswers
+      }
+      val userAnswersWithThirdPartyPackaging = userAnswersWithAmountProduced
+      val userAnswersWithOwnBrands = ownBrands match {
+        case Some(true) => userAnswersWithThirdPartyPackaging
+          .set(OperatePackagingSiteOwnBrandsPage, true).success.value
+          .set(HowManyOperatePackagingSiteOwnBrandsPage, LitresInBands(1, 2)).success.value
+        case Some(false) => userAnswersWithThirdPartyPackaging.set(OperatePackagingSiteOwnBrandsPage, false).success.value
+        case None => userAnswersWithThirdPartyPackaging
+      }
+      val userAnswersWithContract = contract match {
+        case Some(true) => userAnswersWithOwnBrands
+          .set(ContractPackingPage, true).success.value
+          .set(HowManyContractPackingPage, LitresInBands(3, 4)).success.value
+        case Some(false) => userAnswersWithOwnBrands.set(ContractPackingPage, false).success.value
+        case None => userAnswersWithOwnBrands
+      }
+      val userAnswersWithImport = imports match {
+        case Some(true) => userAnswersWithContract
+          .set(ImportsPage, true).success.value
+          .set(HowManyImportsPage, LitresInBands(5, 6)).success.value
+        case Some(false) => userAnswersWithContract.set(ImportsPage, false).success.value
+        case None => userAnswersWithContract
+      }
+      userAnswersWithImport
+    }
 
-      running(application) {
-        val request = FakeRequest(GET, ChangeActivityCYAController.onPageLoad.url)
+    def getAmountProducedSection(userAnswers: UserAnswers): Seq[(String, SummaryList)] = Seq(
+      "changeActivity.checkYourAnswers.amountProducedSection" -> SummaryList(Seq(AmountProducedSummary.row(userAnswers)).flatten)
+    )
 
-        val result = route(application, request).value
+    //      TODO: Don't think 3rd party packagers is implemented
+    def getThirdPartyPackagersSection(userAnswers: UserAnswers): Seq[(String, SummaryList)] = Seq.empty
 
-        val view = application.injector.instanceOf[ChangeActivityCYAView]
-//        TODO: This needs to get actual values
-//        val list: Seq[(String, SummaryList)] = Seq.empty
-        val amountProducedSection: Seq[(String, SummaryList)] = Seq(
-          "changeActivity.checkYourAnswers.amountProducedSection" -> SummaryList(Seq(AmountProducedSummary.row(userAnswers)).flatten)
-        )
-        //      TODO: Don't think 3rd party packagers is implemented
-        val thirdPartyPackagersSection: Seq[(String, SummaryList)] = Seq.empty
-        val operatePackingSiteOwnBrandsSection: Seq[(String, SummaryList)] = Seq(
-          "changeActivity.checkYourAnswers.operatePackingSiteOwnBrandsSection" ->
-            OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
-        )
-        val contractPackingSection: Seq[(String, SummaryList)] = Seq(
-          "changeActivity.checkYourAnswers.contractPackingSection" ->
-            ContractPackingSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
-        )
-        val importsSection: Seq[(String, SummaryList)] = Seq(
-          "changeActivity.checkYourAnswers.importsSection" ->
-            ImportsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
-        )
-        val list = amountProducedSection ++ thirdPartyPackagersSection ++ operatePackingSiteOwnBrandsSection ++ contractPackingSection ++ importsSection
+    def getOperatePackingSiteOwnBrandsSection(userAnswers: UserAnswers): Seq[(String, SummaryList)] = Seq(
+      "changeActivity.checkYourAnswers.operatePackingSiteOwnBrandsSection" ->
+        OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
+    )
 
-        status(result) mustEqual OK
-        contentAsString(result) mustEqual view(
-          aSubscription.orgName,
-//          TODO: Need to fix return period
-          "RETURN PERIOD",
-          list,
-          routes.ChangeActivityCYAController.onSubmit
-        )(request, messages(application)).toString
+    def getContractPackingSection(userAnswers: UserAnswers): Seq[(String, SummaryList)] = Seq(
+      "changeActivity.checkYourAnswers.contractPackingSection" ->
+        ContractPackingSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
+    )
+
+    def getImportsSection(userAnswers: UserAnswers): Seq[(String, SummaryList)] = Seq(
+      "changeActivity.checkYourAnswers.importsSection" ->
+        ImportsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = false)
+    )
+
+    val amountProducedValues: Map[String, Option[AmountProduced]] = Map(
+      "amount produced large" -> Some(Large),
+      "amount produced small" -> Some(Small),
+      "amount produced none" -> Some(NoneProduced),
+      "" -> None
+    )
+
+    val thirdPartyPackagingValues: Map[String, Option[Boolean]] = Map("" -> None)
+    val ownBrandsValues: Map[String, Option[Boolean]] = Map("producing own brands" -> Some(true), "not producing own brands" -> Some(false), "" -> None)
+    val contractValues: Map[String, Option[Boolean]] = Map("contract packing" -> Some(true), "not contract packing" -> Some(false), "" -> None)
+    val importValues: Map[String, Option[Boolean]] = Map("importing" -> Some(true), "not importing" -> Some(false), "" -> None)
+
+//    TODO: Perhaps only need a selection of these to show behaviour
+    val userAnswersMap: Map[String, UserAnswers] =
+      amountProducedValues.flatMap(amountProduced => {
+        thirdPartyPackagingValues.flatMap(thirdPartyPackagingValue => {
+          ownBrandsValues.flatMap(ownBrandsValue => {
+            contractValues.flatMap(contractValue => {
+              importValues.map(importValue => {
+                List(amountProduced._1, thirdPartyPackagingValue._1, ownBrandsValue._1, contractValue._1, importValue._1).filterNot(_.isEmpty).mkString(", ") ->
+                  getUserAnswers(amountProduced._2, thirdPartyPackagingValue._2, ownBrandsValue._2, contractValue._2, importValue._2)
+              })
+            })
+          })
+        })
+      })
+
+    userAnswersMap.foreach { case (key, userAnswers) =>
+      s"must return OK and the correct view for a GET for user answers $key" in {
+
+        val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+        running(application) {
+          val request = FakeRequest(GET, ChangeActivityCYAController.onPageLoad.url)
+
+          val result = route(application, request).value
+
+          val view = application.injector.instanceOf[ChangeActivityCYAView]
+          val list = getAmountProducedSection(userAnswers) ++
+            getThirdPartyPackagersSection(userAnswers) ++
+            getOperatePackingSiteOwnBrandsSection(userAnswers) ++
+            getContractPackingSection(userAnswers) ++
+            getImportsSection(userAnswers)
+
+          status(result) mustEqual OK
+          contentAsString(result) mustEqual view(
+            aSubscription.orgName,
+            //          TODO: Need to fix return period
+            "RETURN PERIOD",
+            list,
+            routes.ChangeActivityCYAController.onSubmit
+          )(request, messages(application)).toString
+        }
       }
     }
 
