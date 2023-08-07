0ba8006c5091b43008e9450f4d4345dfa4ad36ee
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2023-08-07 14:15:45 +0100

    DLS-8579 update wording of change link hidden text

--- app/views/summary/updateRegisteredDetails/BusinessAddressSummary.scala
+++ app/views/summary/updateRegisteredDetails/BusinessAddressSummary.scala
@@ -43,7 +43,7 @@ object BusinessAddressSummary  {
           ),
           actions = Some(Actions("", Seq(
             ActionItemViewModel("site.change", controllers.routes.IndexController.onPageLoad.url)
-              .withVisuallyHiddenText(messages("updateRegisteredDetails.businessAddress.change.hidden", businessAddress.lines.head))
+              .withVisuallyHiddenText(messages("updateRegisteredDetails.businessAddress.change.hidden"))
           )))
         )
     }
