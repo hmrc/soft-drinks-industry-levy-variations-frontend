571cd8c80714ef0462ae6a47aaea78edf64fe369
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-08-14 09:04:18 +0100

    DLS-7972-CS updated ReturnPeriod model

---
+++ test/models/ReturnPeriodSpec.scala
@@ -0,0 +1,73 @@
+package models
+
+import base.SpecBase
+
+import java.time.LocalDate
+
+class ReturnPeriodSpec extends SpecBase with DataHelper {
+
+  "ReturnPeriod" - {
+    "start returns the start date of the quarter" in {
+      val firstSdilReturnPeriod = testReturnPeriod(2018, 1)
+      val quarter1 = testReturnPeriod(year = 2023, quarter = 0)
+      val quarter2 = testReturnPeriod(year = 2023, quarter = 1)
+      val quarter3 = testReturnPeriod(year = 2023, quarter = 2)
+      val quarter4 = testReturnPeriod(year = 2023, quarter = 3)
+
+      firstSdilReturnPeriod.start mustBe LocalDate.of(2018, 4, 5)
+      quarter1.start mustBe LocalDate.of(2023, 1, 1)
+      quarter2.start mustBe LocalDate.of(2023, 4, 1)
+      quarter3.start mustBe LocalDate.of(2023, 7, 1)
+      quarter4.start mustBe LocalDate.of(2023, 10, 1)
+    }
+
+    "end returns the end date of the quarter" in {
+      val quarter1 = testReturnPeriod(year = 2023, quarter = 0)
+      val quarter2 = testReturnPeriod(year = 2023, quarter = 1)
+      val quarter3 = testReturnPeriod(year = 2023, quarter = 2)
+      val quarter4 = testReturnPeriod(year = 2023, quarter = 3)
+
+      quarter1.end mustBe LocalDate.of(2023, 3, 31)
+      quarter2.end mustBe LocalDate.of(2023, 6, 30)
+      quarter3.end mustBe LocalDate.of(2023, 9, 30)
+      quarter4.end mustBe LocalDate.of(2023, 12, 31)
+    }
+
+    "deadline returns the end deadline of the quarter" in {
+      val quarter1 = testReturnPeriod(year = 2023, quarter = 0)
+      val quarter2 = testReturnPeriod(year = 2023, quarter = 1)
+      val quarter3 = testReturnPeriod(year = 2023, quarter = 2)
+      val quarter4 = testReturnPeriod(year = 2023, quarter = 3)
+
+      quarter1.deadline mustBe LocalDate.of(2023, 4, 30)
+      quarter2.deadline mustBe LocalDate.of(2023, 7, 30)
+      quarter3.deadline mustBe LocalDate.of(2023, 10, 30)
+      quarter4.deadline mustBe LocalDate.of(2024, 1, 30)
+    }
+
+    "next returns the next Return" in {
+      val quarter0 = testReturnPeriod(2023, 0)
+      val quarter1 = testReturnPeriod(year = 2023, quarter = 1)
+      val quarter2 = testReturnPeriod(year = 2023, quarter = 2)
+      val quarter3 = testReturnPeriod(year = 2023, quarter = 3)
+
+      quarter0.next mustBe ReturnPeriod(2022, 1)
+      quarter1.next mustBe ReturnPeriod(2023, 2)
+      quarter2.next mustBe ReturnPeriod(2023, 3)
+      quarter3.next mustBe ReturnPeriod(2024, 0)
+    }
+
+    "previous returns the Previous Return" in {
+      val quarter0 = testReturnPeriod(2023, 0)
+      val quarter1 = testReturnPeriod(year = 2023, quarter = 1)
+      val quarter2 = testReturnPeriod(year = 2023, quarter = 2)
+      val quarter3 = testReturnPeriod(year = 2023, quarter = 3)
+
+      quarter0.previous mustBe ReturnPeriod(2022, 3)
+      quarter1.previous mustBe ReturnPeriod(2023, 0)
+      quarter2.previous mustBe ReturnPeriod(2023, 1)
+      quarter3.previous mustBe ReturnPeriod(2023, 2)
+    }
+  }
+
+}
