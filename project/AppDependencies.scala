ccedfc5fce14009495948a3efe8b78c20477edc4
Chris Hill <26716179+ChrisHillCap@users.noreply.github.com>
2023-08-15 15:16:14 +0100

    DLS-8616 added in csp config

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
   import play.core.PlayVersion
 
-  val hmrcMongoVersion = "1.2.0"
+  val hmrcMongoVersion = "1.3.0"
   val playSuffix = s"-play-28"
-  val bootstrapVersion = "7.15.0"
+  val bootstrapVersion = "7.21.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.7.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % bootstrapVersion,
     "org.typelevel"     %% "cats-core"                      % "2.9.0",
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"         % hmrcMongoVersion,
@@ -18,14 +18,14 @@ object AppDependencies {
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                    % "3.2.15",
+    "org.scalatest"           %% "scalatest"                    % "3.2.16",
     "org.scalatestplus"       %% "scalacheck-1-15"              % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"                  % "3.2.10.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"           % "5.1.0",
     "org.pegdown"             %  "pegdown"                      % "1.6.0",
     "org.jsoup"               %  "jsoup"                        % "1.15.4",
     "com.typesafe.play"       %% "play-test"                    % PlayVersion.current,
-    "org.mockito"             %% "mockito-scala"                % "1.17.12",
+    "org.mockito"             %% "mockito-scala"                % "1.17.14",
     "org.scalacheck"          %% "scalacheck"                   % "1.17.0",
     "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"   % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"  % hmrcMongoVersion,
