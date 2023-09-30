afc4f5eaf803a79d29c04e65d0d1501762f50afd
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-09-30 21:18:54 +0100

    DLS-8827: Initial commit for adding UK site details to update registered details CYA controller

--- app/controllers/updateRegisteredDetails/UpdateRegisteredDetailsCYAController.scala
+++ app/controllers/updateRegisteredDetails/UpdateRegisteredDetailsCYAController.scala
@@ -21,10 +21,10 @@ import controllers.actions.ControllerActions
 import models.SelectChange.UpdateRegisteredDetails
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{SummaryList, SummaryListRow}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.updateRegisteredDetails.UpdateRegisteredDetailsCYAView
-import views.summary.updateRegisteredDetails.{BusinessAddressSummary, UpdateContactDetailsSummary}
+import views.summary.updateRegisteredDetails.{BusinessAddressSummary, PackagingSiteDetailsSummary, UpdateContactDetailsSummary, WarehouseDetailsSummary}
 
 import java.time.{LocalDateTime, ZoneId}
 
@@ -37,9 +37,17 @@ class UpdateRegisteredDetailsCYAController @Inject()(
 
   def onPageLoad: Action[AnyContent] = controllerActions.withRequiredJourneyData(UpdateRegisteredDetails) {
     implicit request =>
+//      TODO: Below two are getting answers - not list values
+      val packagingSiteDetailsSummary: Option[SummaryListRow] = PackagingSiteDetailsSummary.row(request.userAnswers)
+      val warehousesDetailsSummary: Option[SummaryListRow] = WarehouseDetailsSummary.row(request.userAnswers)
+      val ukSiteDetailsSummary: Option[(String, SummaryList)] = Option(
+//        TODO: PUT IN MESSAGES
+        "updateRegisteredDetails.checkYourAnswers.ukSiteDetails.title" ->
+          SummaryList(rows = List(packagingSiteDetailsSummary, warehousesDetailsSummary).flatten)
+      )
       val updateContactDetailsSummary: Option[(String, SummaryList)] = UpdateContactDetailsSummary.rows(request.userAnswers)
       val businessAddressSummary: Option[(String, SummaryList)] = BusinessAddressSummary.rows(request.userAnswers)
-      val summaryList = Seq(updateContactDetailsSummary, businessAddressSummary).flatten
+      val summaryList = Seq(ukSiteDetailsSummary, updateContactDetailsSummary, businessAddressSummary).flatten
 
       Ok(view(summaryList, routes.UpdateRegisteredDetailsCYAController.onSubmit))
   }
