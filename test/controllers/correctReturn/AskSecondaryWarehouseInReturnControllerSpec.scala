237e4ac914d61d6e0ec846f00ed7879c4663471c
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2023-11-16 16:01:10 +0000

    DLS-9334: Attempt to fix failing unit tests

--- test/controllers/correctReturn/AskSecondaryWarehouseInReturnControllerSpec.scala
+++ test/controllers/correctReturn/AskSecondaryWarehouseInReturnControllerSpec.scala
@@ -164,7 +164,7 @@ class AskSecondaryWarehouseInReturnControllerSpec extends SpecBase with MockitoS
         withCaptureOfLoggingFrom(application.injector.instanceOf[GenericLogger].logger) { events =>
           val request =
             FakeRequest(POST, askSecondaryWarehouseInReturnRoute)
-          .withFormUrlEncodedBody(("value", "true"))
+          .withFormUrlEncodedBody(("value", "false"))
 
           await(route(application, request).value)
           events.collectFirst {
