1c6be6c932da705bcec5006db1afffc55dc17b30
Drew Miley <drew.miley@digital.hmrc.gov.uk>
2024-01-08 10:42:25 +0000

    DLS-9663: Play 3.0 upgrade (#256)
    * DLS-9663: Updates dependencies for play 3.0.0 - system runs, but tests do not pass
    
    * DLS-9663: Fixes view spec helper
    
    * DLS-9663: Cleans up plugins and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,37 +1,29 @@
 import sbt._
 
 object AppDependencies {
-  import play.core.PlayVersion
 
-  val hmrcMongoVersion = "1.3.0"
-  val playSuffix = s"-play-28"
-  val bootstrapVersion = "7.21.0"
+  private val playSuffix = "-play-30"
+  private val bootstrapVersion = "8.3.0"
+  private val hmrcMongoVersion = "1.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "7.19.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.13.0-play-28",
-    "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix" % bootstrapVersion,
-    "org.typelevel"     %% "cats-core"                      % "2.9.0",
-    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"         % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% s"crypto-json$playSuffix"        % "7.3.0"
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playSuffix"            % "8.2.0",
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playSuffix" % "2.0.0",
+    "uk.gov.hmrc"       %% s"crypto-json$playSuffix"                   % "7.6.0",
+    "uk.gov.hmrc"       %% s"bootstrap-frontend$playSuffix"            % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playSuffix"                    % hmrcMongoVersion,
+    "org.typelevel"     %% "cats-core"                                 % "2.10.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                    % "3.2.16",
-    "org.scalatestplus"       %% "scalacheck-1-15"              % "3.2.11.0",
-    "org.scalatestplus"       %% "mockito-3-4"                  % "3.2.10.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"           % "5.1.0",
-    "org.pegdown"             %  "pegdown"                      % "1.6.0",
-    "org.jsoup"               %  "jsoup"                        % "1.15.4",
-    "com.typesafe.play"       %% "play-test"                    % PlayVersion.current,
-    "org.mockito"             %% "mockito-scala"                % "1.17.14",
-    "org.scalacheck"          %% "scalacheck"                   % "1.17.0",
-    "uk.gov.hmrc"             %% s"bootstrap-test$playSuffix"   % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test$playSuffix"  % hmrcMongoVersion,
-    "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.0",
-    "com.github.tomakehurst"  % "wiremock-standalone"           % "2.27.2",
-    "io.github.wolfendale"    %% "scalacheck-gen-regexp"        % "1.1.0"
+    "org.jsoup"              %  "jsoup"                       % "1.15.4",
+    "org.mockito"            %% "mockito-scala-scalatest"     % "1.17.30",
+    "org.scalatestplus"      %% "scalacheck-1-17"             % "3.2.17.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.0",
+    "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test$playSuffix" % hmrcMongoVersion,
+    "io.github.wolfendale"   %% "scalacheck-gen-regexp"       % "1.1.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
