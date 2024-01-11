79d00dd2eddda188d207df692c166100160d91c1
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-01-11 17:08:48 +0000

    DLS-9743: Corrects requiredPages to be in right files

--- app/pages/correctReturn/CorrectReturnCheckChangesPage.scala
+++ app/pages/correctReturn/CorrectReturnCheckChangesPage.scala
@@ -27,86 +27,11 @@ case object CorrectReturnCheckChangesPage extends Page {
   override def toString: String = "checkChanges"
   override val url: Mode => String = _ => routes.CorrectReturnCheckChangesCYAController.onPageLoad.url
 
-  //    private[controllers] def smallProducerCheck(subscription: RetrievedSubscription): List[CorrectReturnRequiredPage[_, _, _]] = {
-  //    if (subscription.activity.smallProducer) {
-  //      List.empty
-  //    } else {
-  //      List(CorrectReturnRequiredPage(OperatePackagingSiteOwnBrandsPage, None)(implicitly[Reads[Boolean]]),
-  //        CorrectReturnRequiredPage(HowManyOperatePackagingSiteOwnBrandsPage,
-  //          Some(CorrectReturnPreviousPage(OperatePackagingSiteOwnBrandsPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]]))
-  //    }
-  //  }
-  private def smallProducerRequiredPages(userAnswers: UserAnswers, subscription: RetrievedSubscription): List[RequiredPage] = {
-    List.empty
-  }
-
-  //    private[controllers] def addASmallProducerReturnChange(userAnswers: UserAnswers): List[CorrectReturnRequiredPage[_, _, _]] = {
-  //    if (userAnswers.smallProducerList.isEmpty) {
-  //      List(CorrectReturnRequiredPage(AddASmallProducerPage,
-  //        Some(CorrectReturnPreviousPage(ExemptionsForSmallProducersPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[AddASmallProducer]]))
-  //    } else {
-  //      List.empty
-  //    }
-  //  }
-  private def addASmallProducerRequiredPages(userAnswers: UserAnswers, subscription: RetrievedSubscription): List[RequiredPage] = {
-    List.empty
-  }
-
-  //  private[controllers] def packingListReturnChange(
-  //                                                    userAnswers: UserAnswers,
-  //                                                    subscription: RetrievedSubscription
-  //                                                  ): List[CorrectReturnRequiredPage[_, _, _]] = {
-  //    if (UserTypeCheck.isNewPacker(userAnswers, subscription) && subscription.productionSites.isEmpty) {
-  //      List(CorrectReturnRequiredPage(PackAtBusinessAddressPage, None)(implicitly[Reads[Boolean]]))
-  //    } else {
-  //      List.empty
-  //    }
-  //  }
-  private def packagingRequiredPages(userAnswers: UserAnswers, subscription: RetrievedSubscription): List[RequiredPage] = {
-    List.empty
-  }
-
-  //  private[controllers] def warehouseListReturnChange(
-  //                                                      userAnswers: UserAnswers,
-  //                                                      subscription: RetrievedSubscription
-  //                                                    ): List[CorrectReturnRequiredPage[_, _, _]] = {
-  //    if (UserTypeCheck.isNewImporter(userAnswers, subscription)) {
-  //      List(CorrectReturnRequiredPage(AskSecondaryWarehouseInReturnPage, None)(implicitly[Reads[Boolean]]))
-  //    } else {
-  //      List.empty
-  //    }
-  //  }
-  private def warehouseRequiredPages(userAnswers: UserAnswers, subscription: RetrievedSubscription): List[RequiredPage] = {
-    List.empty
-  }
-
-  //  TODO: IMPLEMENT THIS
-  override val previousPagesRequired: (UserAnswers, RetrievedSubscription) => List[RequiredPage] = (userAnswers, subscription) => {
-    //    val firstPartOfRestOfJourney = List(CorrectReturnRequiredPage(PackagedAsContractPackerPage, None)(implicitly[Reads[Boolean]]),
-    //      CorrectReturnRequiredPage(HowManyPackagedAsContractPackerPage,
-    //        Some(CorrectReturnPreviousPage(PackagedAsContractPackerPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]]),
-    //      CorrectReturnRequiredPage(ExemptionsForSmallProducersPage, None)(implicitly[Reads[Boolean]])
-    //    )
-    val firstPartOfJourney = List.empty
-    //    val secondPartOfRestOfJourney = List(CorrectReturnRequiredPage(BroughtIntoUKPage, None)(implicitly[Reads[Boolean]]),
-    //      CorrectReturnRequiredPage(HowManyBroughtIntoUKPage,
-    //        Some(CorrectReturnPreviousPage(BroughtIntoUKPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]]),
-    //      CorrectReturnRequiredPage(BroughtIntoUkFromSmallProducersPage, None)(implicitly[Reads[Boolean]]),
-    //      CorrectReturnRequiredPage(HowManyBroughtIntoUkFromSmallProducersPage,
-    //        Some(CorrectReturnPreviousPage(BroughtIntoUkFromSmallProducersPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]]),
-    //      CorrectReturnRequiredPage(ClaimCreditsForExportsPage, None)(implicitly[Reads[Boolean]]),
-    //      CorrectReturnRequiredPage(HowManyClaimCreditsForExportsPage,
-    //        Some(CorrectReturnPreviousPage(ClaimCreditsForExportsPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]]),
-    //      CorrectReturnRequiredPage(ClaimCreditsForLostDamagedPage, None)(implicitly[Reads[Boolean]]),
-    //      CorrectReturnRequiredPage(HowManyCreditsForLostDamagedPage,
-    //        Some(CorrectReturnPreviousPage(ClaimCreditsForLostDamagedPage, true)(implicitly[Reads[Boolean]])))(implicitly[Reads[LitresInBands]])
-    //    )
-    val secondPartOfJourney = List.empty
-    smallProducerRequiredPages(userAnswers, subscription) ++
-      firstPartOfJourney ++
-      addASmallProducerRequiredPages(userAnswers, subscription) ++
-      secondPartOfJourney ++
-      packagingRequiredPages(userAnswers, subscription) ++
-      warehouseRequiredPages(userAnswers, subscription)
+  override val previousPagesRequired: (UserAnswers, RetrievedSubscription) => List[RequiredPage] = (userAnswers, _) => {
+    val balanceRepaymentRequiredJourney = userAnswers.get(BalanceRepaymentRequired) match {
+      case Some(true) => List(RequiredPage(RepaymentMethodPage))
+      case _ => List.empty
+    }
+    List(RequiredPage(CorrectionReasonPage)) ++ balanceRepaymentRequiredJourney
   }
 }
