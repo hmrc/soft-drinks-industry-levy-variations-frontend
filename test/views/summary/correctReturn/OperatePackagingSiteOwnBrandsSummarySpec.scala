24a9693a5c9485fa62f083842ba7fa9237b26b30
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-05-07 15:41:31 +0100

    DLS-11198: Adds unit tests for pre 2025 and 2025 tax year in OperatePackagingSiteOwnBrandsSummarySpec

--- test/views/summary/correctReturn/OperatePackagingSiteOwnBrandsSummarySpec.scala
+++ test/views/summary/correctReturn/OperatePackagingSiteOwnBrandsSummarySpec.scala
@@ -17,129 +17,148 @@
 package views.summary.correctReturn
 
 import base.SpecBase
-import models.LitresInBands
+import models.{LitresInBands, ReturnPeriod}
 import pages.correctReturn.{HowManyOperatePackagingSiteOwnBrandsPage, OperatePackagingSiteOwnBrandsPage}
 import play.twirl.api.Html
 import uk.gov.hmrc.govukfrontend.views.Aliases.Actions
 
 class OperatePackagingSiteOwnBrandsSummarySpec extends SpecBase {
 
-//  TODO: Split unit tests into "pre April 2025 rates" and "2025 tax year rates" in this file
-
   "summaryList" - {
     val lowLitres = 1000
     val highLitres = 2000
     val includeLevyRowsOptions = List(true, false)
-    includeLevyRowsOptions.foreach(includeLevyRows => {
-      s"should return correct elements when passed in with TRUE and litres provided and check answers is true and include levy rows $includeLevyRows" in {
-        val userAnswers = emptyUserAnswersForCorrectReturn
-          .set(OperatePackagingSiteOwnBrandsPage, true).success.value
-          .set(HowManyOperatePackagingSiteOwnBrandsPage, LitresInBands(lowLitres, highLitres)).success.value
 
-        val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
-        res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
-        res.rows.head.key.classes mustBe ""
-        res.rows.head.value.content.asHtml mustBe Html("Yes")
-        res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows.head.actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-own-brands-packaged-at-own-sites"
-        res.rows.head.actions.head.items.head.attributes mustBe Map("id" -> "change-operatePackagingSiteOwnBrands")
-        res.rows.head.actions.head.items.head.content.asHtml mustBe Html("Change")
+    val preApril2025ReturnPeriod = ReturnPeriod(2025, 0)
+    val taxYear2025ReturnPeriod = ReturnPeriod(2026, 0)
 
-        res.rows(1).key.content.asHtml mustBe Html("Litres in the low band")
-        res.rows(1).key.classes mustBe ""
-        res.rows(1).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(lowLitres))
-        res.rows(1).value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows(1).actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-how-many-own-brands-packaged-at-own-sites"
-        res.rows(1).actions.head.items.head.attributes mustBe Map("id" -> "change-lowband-litreage-operatePackagingSiteOwnBrands")
-        res.rows(1).actions.head.items.head.content.asHtml mustBe Html("Change")
+    def lowBandLevyValue(returnPeriod: ReturnPeriod) = returnPeriod match {
+      case ReturnPeriod(2025, 0) => "£180.00"
+      case ReturnPeriod(2026, 0) => "£194.00"
+    }
 
-        val highLitresRowIndex = if (includeLevyRows) 3 else 2
+    def highBandLevyValue(returnPeriod: ReturnPeriod) = returnPeriod match {
+      case ReturnPeriod(2025, 0) => "£480.00"
+      case ReturnPeriod(2026, 0) => "£518.00"
+    }
 
-        res.rows(highLitresRowIndex).key.content.asHtml mustBe Html("Litres in the high band")
-        res.rows(highLitresRowIndex).key.classes mustBe ""
-        res.rows(highLitresRowIndex).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(highLitres))
-        res.rows(highLitresRowIndex).value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows(highLitresRowIndex).actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-how-many-own-brands-packaged-at-own-sites"
-        res.rows(highLitresRowIndex).actions.head.items.head.attributes mustBe Map("id" -> "change-highband-litreage-operatePackagingSiteOwnBrands")
-        res.rows(highLitresRowIndex).actions.head.items.head.content.asHtml mustBe Html("Change")
+    val returnPeriods = List(
+      (preApril2025ReturnPeriod, "- pre April 2025 rates"),
+      (taxYear2025ReturnPeriod, "- 2025 tax year rates")
+    )
 
-        if (includeLevyRows) {
+    returnPeriods.foreach(returnPeriod => {
+      includeLevyRowsOptions.foreach(includeLevyRows => {
+        s"should return correct elements when passed in with TRUE and litres provided and check answers is true and include levy rows $includeLevyRows ${returnPeriod._2}" in {
+          val userAnswers = emptyUserAnswersForCorrectReturn.copy(correctReturnPeriod = Some(returnPeriod._1))
+            .set(OperatePackagingSiteOwnBrandsPage, true).success.value
+            .set(HowManyOperatePackagingSiteOwnBrandsPage, LitresInBands(lowLitres, highLitres)).success.value
 
-          res.rows(2).key.content.asHtml mustBe Html("Low band levy")
-          res.rows(2).key.classes mustBe ""
-          res.rows(2).value.content.asHtml mustBe Html("£180.00")
-          res.rows(2).value.classes.trim mustBe "sdil-right-align--desktop"
+          val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
+          res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
+          res.rows.head.key.classes mustBe ""
+          res.rows.head.value.content.asHtml mustBe Html("Yes")
+          res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows.head.actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-own-brands-packaged-at-own-sites"
+          res.rows.head.actions.head.items.head.attributes mustBe Map("id" -> "change-operatePackagingSiteOwnBrands")
+          res.rows.head.actions.head.items.head.content.asHtml mustBe Html("Change")
 
-          res.rows(4).key.content.asHtml mustBe Html("High band levy")
-          res.rows(4).key.classes mustBe ""
-          res.rows(4).value.content.asHtml mustBe Html("£480.00")
-          res.rows(4).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows(1).key.content.asHtml mustBe Html("Litres in the low band")
+          res.rows(1).key.classes mustBe ""
+          res.rows(1).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(lowLitres))
+          res.rows(1).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows(1).actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-how-many-own-brands-packaged-at-own-sites"
+          res.rows(1).actions.head.items.head.attributes mustBe Map("id" -> "change-lowband-litreage-operatePackagingSiteOwnBrands")
+          res.rows(1).actions.head.items.head.content.asHtml mustBe Html("Change")
+
+          val highLitresRowIndex = if (includeLevyRows) 3 else 2
+
+          res.rows(highLitresRowIndex).key.content.asHtml mustBe Html("Litres in the high band")
+          res.rows(highLitresRowIndex).key.classes mustBe ""
+          res.rows(highLitresRowIndex).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(highLitres))
+          res.rows(highLitresRowIndex).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows(highLitresRowIndex).actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-how-many-own-brands-packaged-at-own-sites"
+          res.rows(highLitresRowIndex).actions.head.items.head.attributes mustBe Map("id" -> "change-highband-litreage-operatePackagingSiteOwnBrands")
+          res.rows(highLitresRowIndex).actions.head.items.head.content.asHtml mustBe Html("Change")
+
+          if (includeLevyRows) {
+
+            res.rows(2).key.content.asHtml mustBe Html("Low band levy")
+            res.rows(2).key.classes mustBe ""
+            res.rows(2).value.content.asHtml mustBe Html(lowBandLevyValue(returnPeriod._1))
+            res.rows(2).value.classes.trim mustBe "sdil-right-align--desktop"
+
+            res.rows(4).key.content.asHtml mustBe Html("High band levy")
+            res.rows(4).key.classes mustBe ""
+            res.rows(4).value.content.asHtml mustBe Html(highBandLevyValue(returnPeriod._1))
+            res.rows(4).value.classes.trim mustBe "sdil-right-align--desktop"
+          }
+
+          res.rows.size mustBe (if (includeLevyRows) 5 else 3)
         }
+        s"should return correct elements when passed in with TRUE and litres provided and check answers is false and include levy rows $includeLevyRows ${returnPeriod._2}" in {
+          val userAnswers = emptyUserAnswersForCorrectReturn.copy(correctReturnPeriod = Some(returnPeriod._1))
+            .set(OperatePackagingSiteOwnBrandsPage, true).success.value
+            .set(HowManyOperatePackagingSiteOwnBrandsPage, LitresInBands(lowLitres, highLitres)).success.value
 
-        res.rows.size mustBe (if (includeLevyRows) 5 else 3)
-      }
-      s"should return correct elements when passed in with TRUE and litres provided and check answers is false and include levy rows $includeLevyRows" in {
-        val userAnswers = emptyUserAnswersForCorrectReturn
-          .set(OperatePackagingSiteOwnBrandsPage, true).success.value
-          .set(HowManyOperatePackagingSiteOwnBrandsPage, LitresInBands(lowLitres, highLitres)).success.value
+          val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = false, includeLevyRows = includeLevyRows)
+          res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
+          res.rows.head.key.classes mustBe ""
+          res.rows.head.value.content.asHtml mustBe Html("Yes")
+          res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows.head.actions.get mustBe Actions("", List.empty)
 
-        val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = false, includeLevyRows = includeLevyRows)
-        res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
-        res.rows.head.key.classes mustBe ""
-        res.rows.head.value.content.asHtml mustBe Html("Yes")
-        res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows.head.actions.get mustBe Actions("", List.empty)
+          res.rows(1).key.content.asHtml mustBe Html("Litres in the low band")
+          res.rows(1).key.classes mustBe ""
+          res.rows(1).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(lowLitres))
+          res.rows(1).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows(1).actions mustBe None
 
-        res.rows(1).key.content.asHtml mustBe Html("Litres in the low band")
-        res.rows(1).key.classes mustBe ""
-        res.rows(1).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(lowLitres))
-        res.rows(1).value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows(1).actions mustBe None
+          val highLitresRowIndex = if (includeLevyRows) 3 else 2
 
-        val highLitresRowIndex = if (includeLevyRows) 3 else 2
+          res.rows(highLitresRowIndex).key.content.asHtml mustBe Html("Litres in the high band")
+          res.rows(highLitresRowIndex).key.classes mustBe ""
+          res.rows(highLitresRowIndex).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(highLitres))
+          res.rows(highLitresRowIndex).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows(highLitresRowIndex).actions mustBe None
 
-        res.rows(highLitresRowIndex).key.content.asHtml mustBe Html("Litres in the high band")
-        res.rows(highLitresRowIndex).key.classes mustBe ""
-        res.rows(highLitresRowIndex).value.content.asHtml mustBe Html(java.text.NumberFormat.getInstance.format(highLitres))
-        res.rows(highLitresRowIndex).value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows(highLitresRowIndex).actions mustBe None
+          if (includeLevyRows) {
 
-        if (includeLevyRows) {
+            res.rows(2).key.content.asHtml mustBe Html("Low band levy")
+            res.rows(2).key.classes mustBe ""
+            res.rows(2).value.content.asHtml mustBe Html(lowBandLevyValue(returnPeriod._1))
+            res.rows(2).value.classes.trim mustBe "sdil-right-align--desktop"
 
-          res.rows(2).key.content.asHtml mustBe Html("Low band levy")
-          res.rows(2).key.classes mustBe ""
-          res.rows(2).value.content.asHtml mustBe Html("£180.00")
-          res.rows(2).value.classes.trim mustBe "sdil-right-align--desktop"
+            res.rows(4).key.content.asHtml mustBe Html("High band levy")
+            res.rows(4).key.classes mustBe ""
+            res.rows(4).value.content.asHtml mustBe Html(highBandLevyValue(returnPeriod._1))
+            res.rows(4).value.classes.trim mustBe "sdil-right-align--desktop"
+          }
 
-          res.rows(4).key.content.asHtml mustBe Html("High band levy")
-          res.rows(4).key.classes mustBe ""
-          res.rows(4).value.content.asHtml mustBe Html("£480.00")
-          res.rows(4).value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows.size mustBe (if (includeLevyRows) 5 else 3)
         }
+        s"should return correct elements when passed in with FALSE and NO litres provided and include levy rows $includeLevyRows ${returnPeriod._2}" in {
+          val userAnswers = emptyUserAnswersForCorrectReturn.copy(correctReturnPeriod = Some(returnPeriod._1))
+            .set(OperatePackagingSiteOwnBrandsPage, false).success.value
 
-        res.rows.size mustBe (if (includeLevyRows) 5 else 3)
-      }
-      s"should return correct elements when passed in with FALSE and NO litres provided and include levy rows $includeLevyRows" in {
-        val userAnswers = emptyUserAnswersForCorrectReturn
-          .set(OperatePackagingSiteOwnBrandsPage, false).success.value
+          val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
+          res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
+          res.rows.head.key.classes mustBe ""
+          res.rows.head.value.content.asHtml mustBe Html("No")
+          res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
+          res.rows.head.actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-own-brands-packaged-at-own-sites"
+          res.rows.head.actions.head.items.head.attributes mustBe Map("id" -> "change-operatePackagingSiteOwnBrands")
+          res.rows.head.actions.head.items.head.content.asHtml mustBe Html("Change")
 
-        val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
-        res.rows.head.key.content.asHtml mustBe Html("Reporting own brands packaged at your own sites?")
-        res.rows.head.key.classes mustBe ""
-        res.rows.head.value.content.asHtml mustBe Html("No")
-        res.rows.head.value.classes.trim mustBe "sdil-right-align--desktop"
-        res.rows.head.actions.head.items.head.href mustBe "/soft-drinks-industry-levy-variations-frontend/correct-return/change-own-brands-packaged-at-own-sites"
-        res.rows.head.actions.head.items.head.attributes mustBe Map("id" -> "change-operatePackagingSiteOwnBrands")
-        res.rows.head.actions.head.items.head.content.asHtml mustBe Html("Change")
+          res.rows.size mustBe 1
+        }
+        s"should return correct elements when no elements provided and include levy rows $includeLevyRows ${returnPeriod._2}" in {
+          val userAnswers = emptyUserAnswersForCorrectReturn.copy(correctReturnPeriod = Some(returnPeriod._1))
 
-        res.rows.size mustBe 1
-      }
-      s"should return correct elements when no elements provided and include levy rows $includeLevyRows" in {
-        val userAnswers = emptyUserAnswersForCorrectReturn
-
-        val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
-        res.rows.size mustBe 0
-      }
+          val res = OperatePackagingSiteOwnBrandsSummary.summaryList(userAnswers, isCheckAnswers = true, includeLevyRows = includeLevyRows)
+          res.rows.size mustBe 0
+        }
+      })
     })
   }
 }
