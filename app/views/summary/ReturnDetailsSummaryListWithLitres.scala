ef036e1c6b3485cf818911330d0436b4ac57d0f8
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-05-08 15:58:44 +0100

    DLS-11198: Comments out summaryList to see compile errors

--- app/views/summary/ReturnDetailsSummaryListWithLitres.scala
+++ app/views/summary/ReturnDetailsSummaryListWithLitres.scala
@@ -36,17 +36,17 @@ trait ReturnDetailsSummaryListWithLitres extends ReturnDetailsSummaryRowHelper {
   val isSmallProducerLitres: Boolean = false
 
 //  TODO: Separate into changeActivity and correctReturn with and without bandLevyRows - and alter unit tests using these
-  def summaryList(userAnswers: UserAnswers, isCheckAnswers: Boolean, includeLevyRows: Boolean = true)
-                 (implicit messages: Messages, config: FrontendAppConfig): SummaryList = {
-    val litresDetails: Seq[SummaryListRow] = optLitresPage match {
-      case Some(litresPage) => getLitresDetails(userAnswers, isCheckAnswers, litresPage, includeLevyRows)
-      case None if isSmallProducerLitres => getLitresForSmallProducerWithBandLevyRows(userAnswers, isCheckAnswers)
-      case None => Seq.empty
-    }
-    SummaryListViewModel(rows =
-      row(userAnswers, isCheckAnswers) ++ litresDetails
-    )
-  }
+//  def summaryList(userAnswers: UserAnswers, isCheckAnswers: Boolean, includeLevyRows: Boolean = true)
+//                 (implicit messages: Messages, config: FrontendAppConfig): SummaryList = {
+//    val litresDetails: Seq[SummaryListRow] = optLitresPage match {
+//      case Some(litresPage) => getLitresDetails(userAnswers, isCheckAnswers, litresPage, includeLevyRows)
+//      case None if isSmallProducerLitres => getLitresForSmallProducerWithBandLevyRows(userAnswers, isCheckAnswers)
+//      case None => Seq.empty
+//    }
+//    SummaryListViewModel(rows =
+//      row(userAnswers, isCheckAnswers) ++ litresDetails
+//    )
+//  }
 
   def summaryListWithBandLevyRows(userAnswers: UserAnswers, isCheckAnswers: Boolean)
                  (implicit messages: Messages, config: FrontendAppConfig): SummaryList = {
