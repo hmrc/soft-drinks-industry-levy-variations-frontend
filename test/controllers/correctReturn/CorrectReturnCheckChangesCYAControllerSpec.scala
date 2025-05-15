3cba1e6362036e3ae7328e61a565e1944636bc8b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-05-15 16:27:32 +0100

    DLS-11198: Adds failing unit tests for £0.00 for brought into uk from small producers levy rows

--- test/controllers/correctReturn/CorrectReturnCheckChangesCYAControllerSpec.scala
+++ test/controllers/correctReturn/CorrectReturnCheckChangesCYAControllerSpec.scala
@@ -655,14 +655,14 @@ class CorrectReturnCheckChangesCYAControllerSpec extends SpecBase with SummaryLi
         page.getElementById("change-lowband-litreage-broughtIntoUkFromSmallProducers").attributes().get("href") mustEqual
           controllers.correctReturn.routes.HowManyBroughtIntoUkFromSmallProducersController.onPageLoad(CheckMode).url
         page.getElementsByTag("dt").text() must include(Messages("litres.lowBandLevy"))
-        page.getElementsByTag("dd").text() must include("£1,800.00")
+        page.getElementsByTag("dd").text() must include("£0.00")
 
         page.getElementsByTag("dt").text() must include(Messages("litres.highBand"))
         page.getElementsByTag("dd").text() must include("20,000")
         page.getElementById("change-highband-litreage-broughtIntoUkFromSmallProducers").attributes().get("href") mustEqual
           controllers.correctReturn.routes.HowManyBroughtIntoUkFromSmallProducersController.onPageLoad(CheckMode).url
         page.getElementsByTag("dt").text() must include(Messages("litres.highBandLevy"))
-        page.getElementsByTag("dd").text() must include("£4,800.00")
+        page.getElementsByTag("dd").text() must include("£0.00")
       }
     }
 
@@ -695,14 +695,14 @@ class CorrectReturnCheckChangesCYAControllerSpec extends SpecBase with SummaryLi
         page.getElementById("change-lowband-litreage-broughtIntoUkFromSmallProducers").attributes().get("href") mustEqual
           controllers.correctReturn.routes.HowManyBroughtIntoUkFromSmallProducersController.onPageLoad(CheckMode).url
         page.getElementsByTag("dt").text() must include(Messages("litres.lowBandLevy"))
-        page.getElementsByTag("dd").text() must include("£1,940.19")
+        page.getElementsByTag("dd").text() must include("£0.00")
 
         page.getElementsByTag("dt").text() must include(Messages("litres.highBand"))
         page.getElementsByTag("dd").text() must include("20,002")
         page.getElementById("change-highband-litreage-broughtIntoUkFromSmallProducers").attributes().get("href") mustEqual
           controllers.correctReturn.routes.HowManyBroughtIntoUkFromSmallProducersController.onPageLoad(CheckMode).url
         page.getElementsByTag("dt").text() must include(Messages("litres.highBandLevy"))
-        page.getElementsByTag("dd").text() must include("£5,180.52")
+        page.getElementsByTag("dd").text() must include("£0.00")
       }
     }
 
