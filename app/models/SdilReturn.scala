4fc99d3d0458ab5f2ade6f1ee2f597c4638897ab
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-05-21 14:12:32 +0100

    DLS-11199: Adds leviedLitreage and creditedLitreage to SdilReturn model

--- app/models/SdilReturn.scala
+++ app/models/SdilReturn.scala
@@ -34,6 +34,17 @@ case class SdilReturn(
                        submittedOn: Option[LocalDateTime] = None
                      ) {
 //  TODO: total, taxEstimation, and calculateLevy will be updated - can change visibility on calculateLevy
+  private[models] val leviedLitreage: Litreage = Litreage.sum(List(ownBrand, packLarge, importLarge))
+
+  private[models] val creditedLitreage: Litreage = Litreage.sum(List(export, wastage))
+
+  private [models] def calculatelevy(litreage: Litreage)
+                                    (implicit config: FrontendAppConfig): BigDecimal = {
+    val costLower = config.lowerBandCostPerLitre
+    val costHigher = config.higherBandCostPerLitre
+    (litreage.lower * costLower) + (litreage.higher * costHigher)
+  }
+
   def total(implicit config: FrontendAppConfig): BigDecimal = {
     val litresToAdd = Litreage.sum(List(ownBrand, packLarge, importLarge))
     val litresToSubtract = Litreage.sum(List(export, wastage))
@@ -47,39 +58,6 @@ case class SdilReturn(
   def taxEstimation(implicit config: FrontendAppConfig): BigDecimal = {
     val t = Litreage.sum(List(packLarge, importLarge, ownBrand))
     calculatelevy(t.combineN(4))
-  }
-
-//  private[models] def getTotalLowBandLitres(userAnswers: UserAnswers, smallProducer: Boolean): Long = {
-//    val litresPackedAtOwnSite = userAnswers.get(BrandsPackagedAtOwnSitesPage).fold(0L)(_.lowBand)
-//    val litresAsContractPacker = userAnswers.get(HowManyAsAContractPackerPage).fold(0L)(_.lowBand)
-//    val litresBroughtIntoTheUk = userAnswers.get(HowManyBroughtIntoUkPage).fold(0L)(_.lowBand)
-//    val litresExported = userAnswers.get(HowManyCreditsForExportPage).fold(0L)(_.lowBand)
-//    val litresLostOrDamaged = userAnswers.get(HowManyCreditsForLostDamagedPage).fold(0L)(_.lowBand)
-//
-//    val total = litresBroughtIntoTheUk + litresAsContractPacker
-//    val totalCredits = litresExported + litresLostOrDamaged
-//
-//    total - totalCredits + (if (smallProducer) 0 else litresPackedAtOwnSite)
-//  }
-
-//  private[models] def getTotalHighBandLitres(userAnswers: UserAnswers, smallProducer: Boolean): Long = {
-//    val litresPackedAtOwnSite = userAnswers.get(BrandsPackagedAtOwnSitesPage).fold(0L)(_.highBand)
-//    val litresAsContractPacker = userAnswers.get(HowManyAsAContractPackerPage).fold(0L)(_.highBand)
-//    val litresBroughtIntoTheUk = userAnswers.get(HowManyBroughtIntoUkPage).fold(0L)(_.highBand)
-//    val litresExported = userAnswers.get(HowManyCreditsForExportPage).fold(0L)(_.highBand)
-//    val litresLostOrDamaged = userAnswers.get(HowManyCreditsForLostDamagedPage).fold(0L)(_.highBand)
-//
-//    val total = litresBroughtIntoTheUk + litresAsContractPacker
-//    val totalCredits = litresExported + litresLostOrDamaged
-//
-//    total - totalCredits + (if (smallProducer) 0 else litresPackedAtOwnSite)
-//  }
-
-  private [models] def calculatelevy(litreage: Litreage)
-                   (implicit config: FrontendAppConfig): BigDecimal = {
-    val costLower = config.lowerBandCostPerLitre
-    val costHigher = config.higherBandCostPerLitre
-    (litreage.lower * costLower) + (litreage.higher * costHigher)
   }
 }
 
