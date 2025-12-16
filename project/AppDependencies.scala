a69bfb60b79d3b53bcccbf827bbf6f455c61293e
GUJJALAPUDIA <amarnath.gujjalapudi@digital.hmrc.gov.uk>
2025-12-16 16:54:02 +0000

    Update AppDependencies.scala
    pr-bots

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,12 +4,12 @@ object AppDependencies {
 
   private val playVersion = "play-30"
   private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.20.0",
-    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.3.0",
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc-$playVersion"            % "12.22.0",
+    "uk.gov.hmrc"       %% s"play-conditional-form-mapping-$playVersion" % "3.4.0",
     "uk.gov.hmrc"       %% s"crypto-json-$playVersion"                   % "8.4.0",
     "uk.gov.hmrc"       %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
